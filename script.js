ba512b2ddeafc00d517c170135b12306b82e343b
Pramod Carpenter <115171385+pkarpenter2@users.noreply.github.com>
2023-08-26 12:48:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let displayValue = '0';
+
+function updateDisplay() {
+  document.getElementById('display').textContent = displayValue;
+}
+
+function appendToDisplay(value) {
+  if (displayValue === '0' && value !== '.') {
+    displayValue = value;
+  } else {
+    displayValue += value;
+  }
+  updateDisplay();
+}
+
+function clearDisplay() {
+  displayValue = '0';
+  updateDisplay();
+}
+
+function calculate() {
+  try {
+    displayValue = eval(displayValue).toString();
+    updateDisplay();
+  } catch (error) {
+    displayValue = 'Error';
+    updateDisplay();
+  }
+}
+
+updateDisplay();
